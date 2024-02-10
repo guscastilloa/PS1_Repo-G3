745c63f6e4854499ccf0c7d65d1bc54ece4717fd
Gustavo A. Castillo A <gustavo.castillo.alvarez@gmail.com>
2024-02-10 08:13:05 -0500

    Add file

---
+++ scripts/00_packages.R
@@ -0,0 +1,44 @@
+# AUTHOR: Gustavo Castillo
+
+cat("Run under:", version$version.string)
+
+# requiere pacman
+if(!require(pacman)){install.packages("pacman") ; require(pacman)}
+
+p_load(tidyverse , # tidy-data
+       rio, # read/write data from almost any file format
+       dtplyr, # lazy_dt function for handling large amount of data
+       stringr, # string functions
+       data.table, # function: rbindlist()
+       janitor, # function: clean_names()
+       leaflet, # interactive html maps
+       exactextractr,
+       stars,
+       ggmap, # import statemap options
+       ggpattern,  # pattern fill for ggplot objects
+       kableExtra, # making nice latex tables
+       nngeo, # st_nn to do the distance to the nearest neigbour
+       modelsummary, # summary tables
+       readxl, # Import Excel spreadsheet files
+       fixest, #  Fast Fixed-Effects Estimations
+       viridis, # color pallete
+       ggsn, # north star and scale bar
+       ggpubr, # ggexport function
+       lubridate, # calculate diff between years function
+       gridExtra, # grid.arrange function 
+       spdep,
+       haven, # Import stata databases
+       labelled, # tools for labelled data
+       rlang, # Toolbos for working with base types and core R features
+       RcppRoll, # For building rolling windows
+       RSelenium, # For scraping,
+       rvest, # for web scraping
+       arsenal, # To compare dataframes
+       tools)
+
+# solve package conflicts
+filter = dplyr::filter
+select = dplyr::select
+`%no%` = Negate(`%in%`)
+scalebar = ggsn::scalebar
+
