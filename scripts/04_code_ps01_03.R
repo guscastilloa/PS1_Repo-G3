5a02ba3becfbec4bf8d3158d2b6d0e675ee748f7
Alexander Almeida-Ramírez <a.almeidar@uniandes.edu.co>
2024-02-28 16:35:28 -0500

    Update 04_code_ps01_03.R

--- scripts/04_code_ps01_03.R
+++ scripts/04_code_ps01_03.R
@@ -93,3 +93,95 @@ geih_select<-geih_select %>%
 #Generar nuevas variables (edad al cuadrado) y el los salarios en logaritmo. 
 geih_select<- geih_select  %>% mutate(age2=age^2)
 geih_select <- geih_select  %>% mutate(ln_wage = log(y_total_m_ha))
+
+#ESTADÍSTICAS DESCRIPTIVAS ----------------------------------------------------
+#Descriptive statistics of continuos and dicotomic variables
+summary_table <- stargazer(data.frame(geih_select), exclude = c("oficio", "relab"), 
+                           title = "Variables incluidas en nuestra muestra seleccionada", 
+                           align = TRUE, omit.stat = c("n"))
+
+#Export descriptive analysis of selected variables in latex
+writeLines(summary_table, "stores/summary_table.tex")
+
+#Descriptive statistics of categorical variables
+maxEducLevel<-ggplot(geih_select, aes(x = `maxEducLevel`)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Frecuency analysis of the Maximum Educational Level Attained")
+# Export ggplot as PNG
+ggsave("/stores/maxEducLevel.png", plot = maxEducLevel, width = 6, height = 4,
+       dpi = 300)
+
+oficio<-ggplot(geih_select, aes(x = `oficio`)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Frecuency analysis of the Occupation")
+# Export ggplot as PNG
+ggsave("stores/oficio.png", plot = oficio, width = 6, height = 4, dpi = 300)
+
+relab<-ggplot(geih_select, aes(x = `relab`)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Frecuency analysis of the type of occupation")
+# Export ggplot as PNG
+ggsave("stores/relab.png", plot = relab, width = 6, height = 4, dpi = 300)
+
+
+
+
+#REGRESIÓN : log(wage) = b1 + b2(age) + b3(age)^2 + u -------------------------
+#Regress
+reg_age <- lm(ln_wage ~ age + age2, geih_select)
+summary(reg_age)
+
+#Generate the LaTeX code using the stargazer function and store it in a variable
+regression_table <- stargazer(reg_age, title = "Regression Results", align = TRUE, omit.stat = c("ser", "f", "adj.rsq"))
+
+
+
+
+#BOOTSTRAP to construct the confidence intervals -------------------------------
+#Define a function that will extract the coefficients from the model based on bootstrap samples
+get_coefficients <- function(geih_selected, indices) {
+  fit <- lm(ln_wage ~ age + age2, data = geih_selected[indices, ])
+  return(coef(fit))
+}
+
+#Use the boot function to perform the bootstrap procedure and calculate the confidence intervals
+##Set stype = "i" to obtain the percentile intervals
+boot_results <- boot(data = geih_selected, statistic = get_coefficients, R = 1000)
+confidence_intervals <- boot.ci(boot_results, type = "perc")
+
+#Obtain the confidence intervals
+confidence_intervals_95 <- confidence_intervals$percent[, 4]
+
+#Plot of the estimated age-earnings profile
+geih_selected <- geih_selected  %>% mutate(yhat=predict(reg_age))
+summ = geih_selected %>%  
+  group_by(
+    age, age2
+  ) %>%  
+  summarize(
+    mean_y = mean(ln_wage),
+    yhat_reg = mean(yhat), .groups="drop"
+  ) 
+
+
+age_earnings_plot <- ggplot(summ) + 
+  geom_line(
+    aes(x = age, y = yhat_reg), 
+    color = "green", size = 1.5
+  ) + 
+  labs(
+    title = "log Wages by Age in the GEIH 2018",
+    x = "Age",
+    y = "log Wages"
+  ) +
+  theme_bw() 
+
+age_earnings_plot <- age_earnings_plot +
+  geom_vline(xintercept = c(34.58, 49.45), linetype = "dashed", color = "red")
+
+# Export ggplot as PNG
+ggsave("stores/age_earnings_plot.png", plot = age_earnings_plot, width = 6, height = 4, dpi = 300)
+
