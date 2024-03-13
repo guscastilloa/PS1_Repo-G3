4af3b0090e0d9c38063bd50100d066903c10d90d
Gustavo A. Castillo A <gustavo.castillo.alvarez@gmail.com>
2024-03-13 04:54:11 -0500

    Finish analysis and writing predictive section

--- scripts/09_predict_earnings.R
+++ scripts/09_predict_earnings.R
@@ -18,9 +18,11 @@ p_load(leaps, stats, MASS, boot, caret, rio, dplyr, arrow, doSNOW,
 # 1. Prepare dataset ----
 ############################################################################-
 # db <- read_parquet("https://github.com/guscastilloa/PS1_Repo-G3/blob/135ee86dcd92c1f0ef52a9fb73c214a1f3e92338/stores/db.parquet")
-db <- read_parquet("stores/db.parquet")
-db <- db %>% unite(unique_id,c(directorio, secuencia_p, orden))
-db <- db %>% select(unique_id,ln_wage, y_total_m_ha, y_ingLab_m_ha,
+db_f <- read_parquet("stores/db.parquet")
+percentile_function<- ecdf(db_f$y_ingLab_m_ha)
+db_f$y_ingLab_m_ha.percentile <- percentile_function(db_f$y_ingLab_m_ha)*100
+db_f <- db_f %>% unite(unique_id,c(directorio, secuencia_p, orden))
+db <- db_f %>% select(unique_id,ln_wage, y_total_m_ha, y_ingLab_m_ha,
                     hoursWorkUsual,age,female,oficio,ocu,maxEducLevel,
                     formal,microEmpresa,p6050,p6210,
                     posicion, sector,exp, age, agesqr, esc)
@@ -113,38 +115,135 @@ print.xtable(xtbl1, booktabs = TRUE, file = "views/validation_set_rmse.tex",
 
 
 ## Prediction error distribution -------
+
+# Take best model and calculate leverage statistic
+lm.fit.1 <- lm(f4v3, data = db)
+db['hatvalues'] <- hatvalues(lm.fit.1)
+db['residuals'] <- lm.fit.1$residuals
+db['std_residuals'] <- studres
+db['cooks_distance'] <- cooks.distance(lm.fit.1)
+
 pred_errors <- (db.test$ln_wage-predict(lm.fit5v, db.test))
 squared_errors <- (db.test$ln_wage-predict(lm.fit5v, db.test))^2
+
+# Define outlier criteria
+# 1. More than |2σ| from the mean
+ul <- mean(db.test$sq_errors)+2*sd(db.test$sq_errors)
+ll <- mean(db.test$sq_errors)-2*sd(db.test$sq_errors)
+# 2. Leverage limit
+leverage_limit <- (length(lm.fit.1$coefficients)+1)/nrow(db)
+# 3. Cook's Distance greater than 1
+
+
 db.test <-  db.test %>% mutate(
   y_hat=predict(lm.fit5v, db.test),
   pred_errors=pred_errors,
   sq_errors=squared_errors,
   type_pred_error=ifelse(pred_errors>0,"Subestimado",
-                         ifelse(pred_errors<0,"Sobreestimado","Perfecto"))
-)
+                         ifelse(pred_errors<0,"Sobreestimado","Perfecto")),
+  hatvalues=db[-train,]$hatvalues,
+  residuals=db[-train,]$residuals,
+  std_residuals=db[-train,]$std_residuals,
+  outlier_2sigma = ifelse(sq_errors<ll | sq_errors>ul ,
+                           "Sí",'No'),
+  out_lvg_and_stdres = ifelse( (std_residuals>3 | std_residuals< -3) &
+                                 (hatvalues>leverage_limit),
+                               "Sí","No"),
+  cooks_dist = db[-train,]$cooks_distance,
+  cooks_gt1 = ifelse(cooks_dist>1,"Sí","No"),
+  cooks_gt4n = ifelse(cooks_dist>4/(nrow(db)),"Sí","No"),
+  cooks_gt4nk1 = ifelse(cooks_dist>4/(nrow(db)-length(coefficients(lm.fit.1))-1),
+                        "Sí","No"),
+  )
 write_parquet(db.test %>% select(unique_id,y_hat,ln_wage, pred_errors, sq_errors,type_pred_error),
               sink = "stores/db_30_test.parquet")
 
-moments::skewness(db.test$squared_errors)
+o <- db.test %>% filter(unique_id %in% check_ids) %>% select(unique_id,esc, age, female,sq_errors, pred_errors)
+o <- inner_join(x=db_f %>% 
+                  filter(unique_id %in% check_ids) %>% 
+                  select(unique_id,y_ingLab_m_ha.percentile, y_ingLab_m_ha),
+                y=o, by='unique_id') %>% 
+  select(unique_id, age, esc, female,y_ingLab_m_ha,y_ingLab_m_ha.percentile,
+         pred_errors, sq_errors)
+
+out_table <- tibble(o[,-1])
+names(out_table) <-  c("Edad", "Años Escolaridad", 'Sexo',"Ing. Lab. por hora",
+                       'Percentil Ing.','Error Predicción','Error²')
+outxtable <- xtable(out_table, caption = "Características de los 8 Outliers")
+print.xtable(outxtable, comment = FALSE, booktabs = TRUE, file = "views/Xoutliers.tex")
+
+# Income Distribucion by error type
+d1 <- ggplot(data =filter(db.test, squared_errors>1.17) ,
+       aes(x=y_ingLab_m_ha))+
+  geom_density()+
+  theme_bw()+
+  geom_vline(data= data.frame(xint=quantile(db$y_ingLab_m_ha, probs = c(0.05)),
+                              type_pred_error='Sobreestimado'),
+             aes(xintercept = xint),
+             linetype='dotted',color='red')+
+  geom_vline(data= data.frame(xint=quantile(db$y_ingLab_m_ha, probs = c(0.98)),
+                              type_pred_error='Subestimado'),
+             aes(xintercept = xint),
+             linetype='dotted',color='blue')+
+  
+  facet_wrap(~type_pred_error, scales = 'free')+
+  labs(x='Ingreso laboral por hora',
+       y = 'Densidad')
+ggsave("views/y_density_by_sqerrorsgt1p17sigma.pdf",
+       plot = d1, width= 5, height = 3)
+
+
+d2 <- ggplot(data =filter(db.test, squared_errors>1.17) ,
+       aes(x=y_ingLab_m_ha))+
+  stat_ecdf()+
+  theme_bw()+
+  geom_vline(data= data.frame(xint=quantile(db$y_ingLab_m_ha, probs = c(0.05)),
+                              type_pred_error='Sobreestimado'),
+             aes(xintercept = xint),
+             linetype='dotted',color='red')+
+  geom_hline(data= data.frame(yint=.60,
+                              type_pred_error='Sobreestimado'),
+             aes(yintercept = yint),
+             linetype='dotted',color='green')+
+  geom_vline(data= data.frame(xint=quantile(db$y_ingLab_m_ha, probs = c(0.98)),
+                              type_pred_error='Subestimado'),
+             aes(xintercept = xint),
+             linetype='dotted',color='blue')+
+  
+  facet_wrap(~type_pred_error, scales = 'free')+
+  labs(x='Ingreso laboral por hora',
+       y = "CDF Empírica",
+       caption = "Solo se toman observaciones cuyos errores al cuadrado superan mu+2sigma~1.17")
+
+ggsave("views/y_ecdf_by_sqerrorsgt1p17sigma.pdf",
+       plot = d2, width= 5, height = 3)
+
+d1n2 <- ggarrange(d1, d2, nrow=2)
+ggsave("views/density_and_ecdf.pdf",
+       plot = d1n2, width= 5, height = 5)
 
 ggplot(data =filter(db.test, squared_errors>1) ,
-       aes(x=y_ingLab_m_ha))+
+       aes(x=hatvalues))+
   geom_density()+
   theme_bw()+
   facet_grid(~type_pred_error, scales = 'free')
 
-
-## Leverage statistics (STUCK)
-lm.fit5v.2 <- caret::train(f4v3,data = db.train, method = 'lm', )
-lm.fit5v.1 <- caret::train(f4v3,data = db, subset = train, method = 'lm')
-lm.fit.1 <- lm(f4v3, data = db, subset = train)
-
-tail(summary(lm.fit.1)$coefficients,2)
-tail(summary(lm.fit5v.1)$coefficients,2)
-
-predict(lm.fit.1, db[-train,])
-predict(lm.fit5v.1, db[-train,])
-predict(lm.fit5v.2, db[-train,])
+## Leverage vs studentized residuals
+p<- ggplot(db.test, aes(x=hatvalues, y=std_residuals, 
+                        color=out_lvg_and_stdres, shape=type_pred_error))+
+  geom_point()+
+  theme_bw()+
+  geom_hline(yintercept = 3, linetype='dotted', color='gray')+
+  geom_hline(yintercept = -3, linetype='dotted', color='gray')+
+  geom_vline(xintercept = leverage_limit, linetype='dotted', color='black')+
+  labs(x='Leverage',
+       y='Residuales Student',
+       color="Potencial Outlier",
+       shape="Error")+
+  theme(legend.position='bottom',legend.box="vertical",legend.margin=margin())+
+  scale_color_manual(values =c("gray", 'blue'))
+ggsave(filename= "views/bestpred-resVleverage.pdf", p,
+       width = 4, height = 5)  
 
 ############################################################################-
 # 2. LOOCV ----
